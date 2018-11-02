c7083f2d9e3792b688c479df259f145c800bf747
chrissydc <bychriscelle@gmail.com>
2018-11-01 21:08:58 -0400

    current score is not being incremented once card has been clicked

--- src/App.js
+++ src/App.js
@@ -3,26 +3,73 @@ import Wrapper from "./Components/Wrapper";
 import Header from "./Components/Header";
 import Title from "./Components/Title";
 import Gamecard from "./Components/GameCard";
-import Footer from "./Components/Footer";
 import pokemon from "./pokemon.json";
 import './App.css';
+
+const shuffle = require("shuffle-array");
 
 class App extends Component {
 
   state = {
-    pokemon
+    images:pokemon,
+    clickedImg: [],
+    current: 0,
+    top: 0,
+    message: "Click an image to start!"
   };
 
-  
+  imageShuffleFn = (id) => {
+    let find = 0;
+    const arrayLength = this.state.images.length;
+
+    for(let i = 0; i < arrayLength; i++) {
+      if (id === this.state.clickedImg[i]) {
+        find = 1;
+        this.setState ({
+          message: "You've done well, keep playing!"
+        });
+
+        this.setState({
+          pokemon,
+          clickedImg: [],
+          current: 0,
+          counter: 0
+        })
+      }
+    }
+
+    if(find===0) {
+      this.state.clickedImg.push(id);
+      var currentScore = this.state.current + 1;
+
+      this.setState({
+        current: currentScore,
+        message: "You've done well, keep playing!",
+        images: shuffle(this.state.images)
+      })
+
+      var topScore = this.state.top;
+      if(topScore <= currentScore) {
+        this.setState({topScore:currentScore})
+      }
+
+    }
+  }
 
 
   render() {
     return (
       <Wrapper>
-        <Header> Clicky Game </Header>
-        <Title>Gotta catch them all, Pokemon!</Title>
-        {this.state.pokemon.map(pokemon => (
+        <Header
+          title="Pokemon Clicky Game"
+          message={this.state.message}
+          current={this.state.current}
+          top={this.state.top}
+        />
+        <Title>Gotta catch them all!</Title>
+        {this.state.images.map(pokemon => (
         <Gamecard
+            imageShuffle={()=> this.imageShuffleFn(pokemon.id)}
             id={pokemon.id}
             key={pokemon.id}
             name={pokemon.name}
